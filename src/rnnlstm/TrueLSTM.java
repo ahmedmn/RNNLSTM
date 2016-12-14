47735d20ecfd8c468b475afa4e3c4f31b3580b5f
Lkorenciak <lkorenciak@gmail.com>
2016-12-14 23:20:07 +0100

    =reuse of matrices

--- src/rnnlstm/TrueLSTM.java
+++ src/rnnlstm/TrueLSTM.java
@@ -73,9 +73,9 @@ public class TrueLSTM {
         
         double alpha = 0.05;
         int hiddenDim = 300;
-        int numOfReviews = 10;
+        int numOfReviews = 20000;
         int numOfIterations = 1000000;
-        int numNotPrintedIters = 1;
+        int numNotPrintedIters = 20000;
         
         int dataRowNum = X_train.length;
         int dataColNum = X_train[0].length;
@@ -181,7 +181,6 @@ public class TrueLSTM {
         }
 
 
-        Random rand = new Random();
 
         double[] x;
         double[][] forgetGate, inputGate, outputGate, memoryInput, memory, output, almostOutput;
@@ -207,7 +206,14 @@ public class TrueLSTM {
         memory[0] = new double[hiddenDim];
         output[0] = new double[hiddenDim];
         
-        double[] futureOutputDelta, currentOutputDelta, futureMemoryDelta, currentMemoryDelta;
+        double[] futureOutputDelta, currentOutputDelta, futureMemoryDelta, currentMemoryDelta, aux;
+        futureOutputDelta = new double[hiddenDim];
+        futureMemoryDelta = new double[hiddenDim];
+        currentOutputDelta = new double[hiddenDim];
+        currentMemoryDelta = new double[hiddenDim];        
+        
+        temp = new double[hiddenDim];
+        temp2 = new double[hiddenDim];
         
         int i, j, k, reviewRow, wordCol, counter =0;
         for (j = 0; j < numOfIterations; j++) {
@@ -234,59 +240,49 @@ public class TrueLSTM {
 
                 //forward pass
                 for (wordCol = 1; wordCol <= dataColNum; wordCol++) {
-                    temp = vector.scalarVectMult(x[wordCol], weightsForget);
-                    vector.addVectors(temp, matrix.vectorMatrixMult(output[wordCol-1], weightsForget2));
-                    vector.vectSigmoidNoOut(temp);
-                    forgetGate[wordCol] = temp;
-                    memory[wordCol] = vector.vectorVectorMultAsterisk(memory[wordCol-1], temp);
+                    vector.scalarVectMultNoOut(forgetGate[wordCol], x[wordCol], weightsForget);
+                    matrix.vectorMatrixMultNoOut(temp2, output[wordCol-1], weightsForget2);
+                    vector.addVectors(forgetGate[wordCol], temp2);
+                    vector.vectSigmoidNoOut(forgetGate[wordCol]);
+                    vector.vectorVectorMultAsteriskNoOut(memory[wordCol], memory[wordCol-1], forgetGate[wordCol]);
 
-                    temp = vector.scalarVectMult(x[wordCol], weightsIn);
-                    vector.addVectors(temp, matrix.vectorMatrixMult(output[wordCol-1], weightsIn2));
-                    vector.vectSigmoidNoOut(temp);
-                    inputGate[wordCol] = temp; //TODO we won't need to create new arrays for temp, we can reuse the old ones and keep them in inputGate[wordCol]
+                    vector.scalarVectMultNoOut(inputGate[wordCol], x[wordCol], weightsIn);
+                    matrix.vectorMatrixMultNoOut(temp2, output[wordCol-1], weightsIn2);
+                    vector.addVectors(inputGate[wordCol], temp2);
+                    vector.vectSigmoidNoOut(inputGate[wordCol]);
 
-                    temp = vector.scalarVectMult(x[wordCol], weightsMemory);
-                    vector.addVectors(temp, matrix.vectorMatrixMult(output[wordCol-1], weightsMemory2));
-                    vector.vectTangentHNoOut(temp);
-                    memoryInput[wordCol] = temp;
+                    vector.scalarVectMultNoOut(memoryInput[wordCol], x[wordCol], weightsMemory);
+                    matrix.vectorMatrixMultNoOut(temp2, output[wordCol-1], weightsMemory2);
+                    vector.addVectors(memoryInput[wordCol], temp2);
+                    vector.vectTangentHNoOut(memoryInput[wordCol]);
 
-                    vector.addVectors(memory[wordCol],
-                            vector.vectorVectorMultAsterisk(inputGate[wordCol],
-                                    memoryInput[wordCol]));
+                    vector.vectorVectorMultAsteriskNoOut(temp2, 
+                                                        inputGate[wordCol],
+                                                        memoryInput[wordCol]);
+                    vector.addVectors(memory[wordCol], temp2);
 
                     vector.copy(almostOutput[wordCol], memory[wordCol]);
                     vector.vectTangentHNoOut(almostOutput[wordCol]);
 
-                    temp = vector.scalarVectMult(x[wordCol], weightsOut);
-                    vector.addVectors(temp, matrix.vectorMatrixMult(output[wordCol-1], weightsOut2));
-                    vector.vectSigmoidNoOut(temp);
-                    outputGate[wordCol] = temp;
+                    vector.scalarVectMultNoOut(outputGate[wordCol], x[wordCol], weightsOut);
+                    matrix.vectorMatrixMultNoOut(temp2, output[wordCol-1], weightsOut2);
+                    vector.addVectors(outputGate[wordCol], temp2);
+                    vector.vectSigmoidNoOut(outputGate[wordCol]);
 
-                    output[wordCol] = vector.vectorVectorMultAsterisk(outputGate[wordCol], almostOutput[wordCol]);
-                    
-//                    double count=0;
-//                    for(double res:forgetGate[wordCol]){
-//                        count += res;
-//                    }
-//                    System.out.print(count + " ");
+                    vector.vectorVectorMultAsteriskNoOut(output[wordCol], outputGate[wordCol], almostOutput[wordCol]);
                 }
-//                System.out.println("");
                 
                 //compute output
                 finalOutput = sigmoid.computeSigmoid(vector.vectorVectorMultDot(output[dataColNum], weightsFinal));
-
-//                finalOutputError = -200 * (y-finalOutput); //Math.pow((y - finalOutput)*10,2)
                 finalOutputError = 2*(y-finalOutput); //Math.pow((y - finalOutput),2)                
                 d[reviewRow] = Math.round(finalOutput);
                 outputs[reviewRow]  = finalOutput;
                 errors[reviewRow]   = finalOutputError;
                 finalOutputDelta = (finalOutputError * sigmoid.sigmoidOutputToDerivative(finalOutput));
 
-                weightsFinalUpdate= vector.scalarVectMult(finalOutputDelta, weightsFinal); //computing dh
+                vector.scalarVectMultNoOut(weightsFinalUpdate, finalOutputDelta, weightsFinal); //computing dh
 
-                futureOutputDelta = weightsFinalUpdate;
-                futureMemoryDelta = new double[hiddenDim];
-
+                vector.copy(futureOutputDelta, weightsFinalUpdate);
                 for (k = 0; k < hiddenDim; k++) {
                     futureMemoryDelta[k] = 0;
                 }
@@ -294,51 +290,110 @@ public class TrueLSTM {
                 //backpropagation
                 for (wordCol = dataColNum; wordCol > 0; wordCol--) {
 
-                    temp = vector.vectorVectorMultAsterisk(futureOutputDelta, almostOutput[wordCol]); //do^t
-                    temp = vector.vectorVectorMultAsterisk(temp, vector.vectSigmoidOutputToDerivative(outputGate[wordCol]));//d(o^)^t
-                    vector.addVectors(weightsOutUpdate, vector.scalarVectMult(x[wordCol], temp)); //TODO check whether it is correct
-                    matrix.addMatrices(weightsOutUpdate2, vector.vectorVectorMultDotM(output[wordCol-1], temp));
+                    for (k = 0; k < hiddenDim; k++) {
+                        currentOutputDelta[k] = 0;
+                        currentMemoryDelta[k] = 0;
+                    }
+                    
+
+                    vector.vectorVectorMultAsteriskNoOut(temp, futureOutputDelta, almostOutput[wordCol]); //do^t
+                    vector.vectSigmoidOutputToDerivativeNoOut(temp2, outputGate[wordCol]);
+                    vector.vectorVectorMultAsteriskNoOut(temp, temp, temp2);//d(o^)^t
+                    vector.scalarVectMultNoOut(temp2, x[wordCol], temp);
+                    vector.addVectors(weightsOutUpdate, temp2);
+                    vector.vectorVectorMultDotMNoOutAdd(weightsOutUpdate2, output[wordCol-1], temp);
                     //computing dh^(t-1)
-                    currentOutputDelta = matrix.vectorMatrixMult(temp, matrix.transpose(weightsOut2));
+                    matrix.vectorMatrixMultNoOutAdd(currentOutputDelta, temp, matrix.transpose(weightsOut2));
 
-                    temp = vector.vectorVectorMultAsterisk(futureOutputDelta, outputGate[wordCol]); // dh^t . o^t
+//                    temp = vector.vectorVectorMultAsterisk(futureOutputDelta, almostOutput[wordCol]); //do^t
+//                    temp = vector.vectorVectorMultAsterisk(temp, vector.vectSigmoidOutputToDerivative(outputGate[wordCol]));//d(o^)^t
+//                    vector.addVectors(weightsOutUpdate, vector.scalarVectMult(x[wordCol], temp)); //TODO check whether it is correct
+//                    matrix.addMatrices(weightsOutUpdate2, vector.vectorVectorMultDotM(output[wordCol-1], temp));
+//                    //computing dh^(t-1)
+//                    currentOutputDelta = matrix.vectorMatrixMult(temp, matrix.transpose(weightsOut2));
+
+                    vector.vectorVectorMultAsteriskNoOut(temp, futureOutputDelta, outputGate[wordCol]); // dh^t . o^t
                     vector.vectTangentHToDerivativeNoOut(almostOutput[wordCol]);
-                    temp = vector.vectorVectorMultAsterisk(temp, almostOutput[wordCol]);
+                    vector.vectorVectorMultAsteriskNoOut(temp, temp, almostOutput[wordCol]);
                     vector.addVectors(futureMemoryDelta, temp); // dc^t
+                    
+//                    temp = vector.vectorVectorMultAsterisk(futureOutputDelta, outputGate[wordCol]); // dh^t . o^t
+//                    vector.vectTangentHToDerivativeNoOut(almostOutput[wordCol]);
+//                    temp = vector.vectorVectorMultAsterisk(temp, almostOutput[wordCol]);
+//                    vector.addVectors(futureMemoryDelta, temp); // dc^t
 
-                    currentMemoryDelta = vector.vectorVectorMultAsterisk(futureMemoryDelta, forgetGate[wordCol]); //dc^(t-1)
+                    vector.vectorVectorMultAsteriskNoOut(currentMemoryDelta, futureMemoryDelta, forgetGate[wordCol]); //dc^(t-1)
+
+//                    currentMemoryDelta = vector.vectorVectorMultAsterisk(futureMemoryDelta, forgetGate[wordCol]); //dc^(t-1)
 
                     //forget gate
-                    temp = vector.vectorVectorMultAsterisk(futureMemoryDelta, memory[wordCol - 1]); //df^t
-                    temp = vector.vectorVectorMultAsterisk(temp, vector.vectSigmoidOutputToDerivative(forgetGate[wordCol]));
-                    vector.addVectors(weightsForgetUpdate, vector.scalarVectMult(x[wordCol], temp)); //TODO check whether it is correct
-                    matrix.addMatrices(weightsForgetUpdate2, vector.vectorVectorMultDotM(output[wordCol-1], temp));
+                    vector.vectorVectorMultAsteriskNoOut(temp, futureMemoryDelta, memory[wordCol - 1]); //df^t
+                    vector.vectSigmoidOutputToDerivativeNoOut(temp2, forgetGate[wordCol]);
+                    vector.vectorVectorMultAsteriskNoOut(temp, temp, temp2);
+                    vector.scalarVectMultNoOut(temp2, x[wordCol], temp);
+                    vector.addVectors(weightsForgetUpdate, temp2);
+                    vector.vectorVectorMultDotMNoOutAdd(weightsForgetUpdate2, output[wordCol-1], temp);
                     //computing dh^(t-1)
-                    vector.addVectors(currentOutputDelta, matrix.vectorMatrixMult(temp, matrix.transpose(weightsForget2)));
+                    matrix.vectorMatrixMultNoOutAdd(currentOutputDelta, temp, matrix.transpose(weightsForget2));
+
+
+//                    //forget gate
+//                    temp = vector.vectorVectorMultAsterisk(futureMemoryDelta, memory[wordCol - 1]); //df^t
+//                    temp = vector.vectorVectorMultAsterisk(temp, vector.vectSigmoidOutputToDerivative(forgetGate[wordCol]));
+//                    vector.addVectors(weightsForgetUpdate, vector.scalarVectMult(x[wordCol], temp)); //TODO check whether it is correct
+//                    matrix.addMatrices(weightsForgetUpdate2, vector.vectorVectorMultDotM(output[wordCol-1], temp));
+//                    //computing dh^(t-1)
+//                    vector.addVectors(currentOutputDelta, matrix.vectorMatrixMult(temp, matrix.transpose(weightsForget2)));
 
                     //input gate
-                    temp = vector.vectorVectorMultAsterisk(futureMemoryDelta, memoryInput[wordCol]); //di^t
-                    temp = vector.vectorVectorMultAsterisk(temp, vector.vectSigmoidOutputToDerivative(inputGate[wordCol]));
-                    vector.addVectors(weightsInUpdate, vector.scalarVectMult(x[wordCol], temp)); //TODO check whether it is correct
-                    matrix.addMatrices(weightsInUpdate2, vector.vectorVectorMultDotM(output[wordCol-1], temp));
+                    vector.vectorVectorMultAsteriskNoOut(temp, futureMemoryDelta, memoryInput[wordCol]); //di^t
+                    vector.vectSigmoidOutputToDerivativeNoOut(temp2, inputGate[wordCol]);
+                    vector.vectorVectorMultAsteriskNoOut(temp, temp, temp2);
+                    vector.scalarVectMultNoOut(temp2, x[wordCol], temp);
+                    vector.addVectors(weightsInUpdate, temp2);
+                    vector.vectorVectorMultDotMNoOutAdd(weightsInUpdate2,output[wordCol-1], temp);
                     //computing dh^(t-1)
-                    vector.addVectors(currentOutputDelta, matrix.vectorMatrixMult(temp, matrix.transpose(weightsIn2)));
+                    matrix.vectorMatrixMultNoOutAdd(currentOutputDelta, temp, matrix.transpose(weightsIn2));
 
+                    
+//                    //input gate
+//                    temp = vector.vectorVectorMultAsterisk(futureMemoryDelta, memoryInput[wordCol]); //di^t
+//                    temp = vector.vectorVectorMultAsterisk(temp, vector.vectSigmoidOutputToDerivative(inputGate[wordCol]));
+//                    vector.addVectors(weightsInUpdate, vector.scalarVectMult(x[wordCol], temp)); //TODO check whether it is correct
+//                    matrix.addMatrices(weightsInUpdate2, vector.vectorVectorMultDotM(output[wordCol-1], temp));
+//                    //computing dh^(t-1)
+//                    vector.addVectors(currentOutputDelta, matrix.vectorMatrixMult(temp, matrix.transpose(weightsIn2)));                    
+                    
                     //memory input
-                    temp = vector.vectorVectorMultAsterisk(futureMemoryDelta, inputGate[wordCol]); //da^t
+                    vector.vectorVectorMultAsteriskNoOut(temp, futureMemoryDelta, inputGate[wordCol]); //da^t
                     vector.vectTangentHToDerivativeNoOut(memoryInput[wordCol]);
-                    temp = vector.vectorVectorMultAsterisk(temp, memoryInput[wordCol]); //d(a^)^t
-                    vector.addVectors(weightsMemoryUpdate, vector.scalarVectMult(x[wordCol], temp)); //TODO check whether it is correct
-                    matrix.addMatrices(weightsMemoryUpdate2, vector.vectorVectorMultDotM(output[wordCol-1], temp));
+                    vector.vectorVectorMultAsteriskNoOut(temp, temp, memoryInput[wordCol]); //d(a^)^t
+                    vector.scalarVectMultNoOut(temp2, x[wordCol], temp);
+                    vector.addVectors(weightsMemoryUpdate, temp2);
+                    vector.vectorVectorMultDotMNoOutAdd(weightsMemoryUpdate2, output[wordCol-1], temp);
                     //computing dh^(t-1)
-                    vector.addVectors(currentOutputDelta, matrix.vectorMatrixMult(temp, matrix.transpose(weightsMemory2)));
+                    matrix.vectorMatrixMultNoOutAdd(currentOutputDelta, temp, matrix.transpose(weightsMemory2));
 
+               
+//                    //memory input
+//                    temp = vector.vectorVectorMultAsterisk(futureMemoryDelta, inputGate[wordCol]); //da^t
+//                    vector.vectTangentHToDerivativeNoOut(memoryInput[wordCol]);
+//                    temp = vector.vectorVectorMultAsterisk(temp, memoryInput[wordCol]); //d(a^)^t
+//                    vector.addVectors(weightsMemoryUpdate, vector.scalarVectMult(x[wordCol], temp)); //TODO check whether it is correct
+//                    matrix.addMatrices(weightsMemoryUpdate2, vector.vectorVectorMultDotM(output[wordCol-1], temp));
+//                    //computing dh^(t-1)
+//                    vector.addVectors(currentOutputDelta, matrix.vectorMatrixMult(temp, matrix.transpose(weightsMemory2)));                    
+                    
                     // preparing new values of memory and output
+                    aux = futureMemoryDelta;
                     futureMemoryDelta = currentMemoryDelta;
+                    currentMemoryDelta = aux;
+                    aux = futureOutputDelta;
                     futureOutputDelta = currentOutputDelta;
+                    currentOutputDelta = aux;
 
                 }
-//                System.out.println("");
+
                 
                 vector.addVectors(weightsIn, vector.scalarVectMult(alpha, weightsInUpdate));
                 vector.addVectors(weightsForget, vector.scalarVectMult(alpha, weightsForgetUpdate));
